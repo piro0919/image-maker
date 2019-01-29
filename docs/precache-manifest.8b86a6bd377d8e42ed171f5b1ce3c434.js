84f31be23644c31b7df7232c6811b53da75e7d6d
PIROSHIKI-PC\piro <piro.haniwa@gmail.com>
2019-01-29 19:19:35 +0900

    add docs

---
+++ docs/precache-manifest.8b86a6bd377d8e42ed171f5b1ce3c434.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5180756861fd5764ea3b",
+    "url": "/image-maker/static/js/main.51807568.chunk.js"
+  },
+  {
+    "revision": "0f6f1faea049df86e0d8",
+    "url": "/image-maker/static/css/1.a0d2b07e.chunk.css"
+  },
+  {
+    "revision": "0f6f1faea049df86e0d8",
+    "url": "/image-maker/static/js/1.0f6f1fae.chunk.js"
+  },
+  {
+    "revision": "79dc00fdd0f9eac2933b",
+    "url": "/image-maker/static/js/runtime~main.79dc00fd.js"
+  },
+  {
+    "revision": "c05d24f3ff255d63351a7df45b81d65d",
+    "url": "/image-maker/index.html"
+  }
+];
